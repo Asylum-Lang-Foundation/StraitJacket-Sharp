0322e2a5712c28bb876375ec52c3c0a7619abaec
Gota7 <gotaseven@gmail.com>
2021-12-26 00:01:28 -0500

    Start Builder
    Progress.

---
+++ Builder/Builder.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+using StraitJacket.Constructs;
+
+namespace StraitJacket.Builder {
+
+    // Asylum program builder.
+    public partial class Builder {
+        Dictionary<string, Constructs.AST> ASTs = new Dictionary<string, Constructs.AST>();
+
+    }
+
+}
---
+++ Builder/File.cs
@@ -0,0 +1,36 @@
+using StraitJacket.Constructs;
+
+namespace StraitJacket.Builder { 
+
+    // Asylum program builder.
+    public partial class Builder {
+        string CurrFile = null;
+        Constructs.AST AST = null;
+
+        // Start appending to a source file.
+        public void BeginFile(string sourcePath) {
+            if (CurrFile != null) throw new System.Exception("Can't begin file: " + sourcePath + " has not been closed.");
+            CurrFile = sourcePath;
+            if (!ASTs.ContainsKey(sourcePath)) {
+                ASTs.Add(sourcePath, new Constructs.AST());
+            }
+            AST = ASTs[sourcePath];
+        }
+
+        // Stop appending to a source file.
+        public void EndFile(string endPath) {
+            FileCheck();
+            CurrFile = null;
+            AST = null;
+        }
+
+        // Check the file.
+        private void FileCheck() {
+            if (AST == null) {
+                throw new System.Exception("There is currently no file open!");
+            }
+        }
+
+    }
+
+}
