6051e3764d6ce89151f81899635e872289772f56
Gota7 <gotaseven@gmail.com>
2022-01-07 17:53:14 -0500

    Update Project Files
    Necessary.

--- test.sh
+++ test.sh
@@ -1,9 +1,9 @@
-mkdir Tests/obj
-mkdir Tests/bin
+mkdir --parents Tests/obj
+mkdir --parents Tests/bin
 rm Tests/obj/$1.bc
 rm Tests/bin/$1.elf
 rm Tests/obj/$1.ll
-dotnet run -p StraitJacket/StraitJacket.csproj $1.asy
+dotnet run --project StraitJacket/StraitJacket.csproj $1.asy
 llvm-dis Tests/obj/$1.bc -o Tests/obj/$1.ll
 clang -O2 Tests/obj/$1.bc -o Tests/bin/$1.elf
 Tests/bin/$1.elf
--- testMac.sh
+++ testMac.sh
@@ -3,7 +3,7 @@ mkdir Tests/bin
 rm Tests/obj/$1.bc
 rm Tests/bin/$1.elf
 rm Tests/obj/$1.ll
-dotnet run -p StraitJacket/StraitJacket.csproj $1.asy
+dotnet run --project StraitJacket/StraitJacket.csproj $1.asy
 llvm-dis Tests/obj/$1.bc -o Tests/obj/$1.ll
 clang -L /Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/lib -O2 Tests/obj/$1.bc -o Tests/bin/$1.elf
 Tests/bin/$1.elf
